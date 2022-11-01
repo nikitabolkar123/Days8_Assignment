4c0f2139ed84f5962a668f825cfc631c7f078011
nikitabolkar123 <bolkarnikita2001@gmail.com>
2022-11-01 06:19:10 -0700

    added uc7

--- PresentAbsent.cs
+++ PresentAbsent.cs
@@ -1,51 +1,44 @@
 ﻿using System;
 
-namespace WorkingHrCondn
+namespace SwitchCase
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            EmpCheack emp = new EmpCheack();
-            emp.EmpWage();
+            Empcheack e = new Empcheack();
+            e.EmpWage();
+
         }
     }
-    public class EmpCheack
-    {
 
+    public class Empcheack
+    {
         public const int isPartTime = 1;
         public const int isFullTime = 2;
         public const int empRatePerHr = 20;
-        public const int noOfWorkingDays = 2;
-        public const int maxHrsInMonth = 10;
 
         public void EmpWage()
         {
-            int empHr = 0, totalEmpHrs = 0, totalWorkingDays = 0;
-            while (totalEmpHrs <= maxHrsInMonth && totalWorkingDays < noOfWorkingDays)
+            int empHr = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            switch (empCheck)
             {
-                totalWorkingDays++;
-                Random random = new Random();
-                int empCheck = random.Next(0, 3);
-                switch (empCheck)
-                {
-                    case isPartTime:
-                        empHr = 4;
-                        break;
-                    case isFullTime:
-                        empHr = 8;
-                        break;
-                    default:
-                        empHr = 0;
-                        break;
-
-                }
-                totalEmpHrs = totalEmpHrs + empHr;
-                Console.WriteLine("Days=" + totalWorkingDays + "Emp Hrs=" + empHr);
+                case isPartTime:
+                    empHr = 4;
+                    break;
+                case isFullTime:
+                    empHr = 8;
+                    break;
+                default:
+                    empHr = 0;
+                    break;
             }
-            int totalEmpWage = totalEmpHrs * empRatePerHr;
-            Console.WriteLine("Total Emp Wage=" + totalEmpWage);
+            empWage = empHr * empRatePerHr;
+
+            Console.WriteLine("EmpWage=" + empWage);
         }
     }
 }
-
