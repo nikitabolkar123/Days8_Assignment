76dd7078003ed187d2c83ff47941a038ecb8679d
nikitabolkar123 <bolkarnikita2001@gmail.com>
2022-11-01 06:21:47 -0700

    added uc8

--- PresentAbsent.cs
+++ PresentAbsent.cs
@@ -1,44 +1,104 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System;
 
-namespace SwitchCase
+public interface IComputeEmpWage
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Empcheack e = new Empcheack();
-            e.EmpWage();
 
+}
+public class CompanyEmpWage
+{
+    public string company;
+    public int Emp_Rate_Per_Hr;
+    public int Num_Of_Working_Days;
+    public int Max_Hrs_Per_Month;
+    public int totalEmpWage;
+    public CompanyEmpWage(String company, int Emp_Rate_Per_Hr, int Num_Of_Working_Days, int Max_Hrs_Per_Month)
+    {
+        this.company = company;
+        this.Emp_Rate_Per_Hr = Emp_Rate_Per_Hr;
+        this.Num_Of_Working_Days = Num_Of_Working_Days;
+        this.Max_Hrs_Per_Month = Max_Hrs_Per_Month;
+        this.totalEmpWage = 0;
+    }
+    public void setTotalEmpWage(int totalEmpWage)
+    {
+        this.totalEmpWage = totalEmpWage;
+    }
+    public string toString()
+    {
+        return "Total Emp Wage for Company : " + this.company + " is: " + this.totalEmpWage;
+    }
+}
+public class EmpWageBuilder
+{
+    public const int IS_PART_TIME = 1;
+    public const int IS_FULL_TIME = 2;
+
+    private LinkedList<CompanyEmpWage> companyEmpWageList;
+    private Dictionary<string, CompanyEmpWage> companyToEmpWageMap;
+    public EmpWageBuilder()
+    {
+        this.companyEmpWageList = new LinkedList<CompanyEmpWage>();
+        this.companyToEmpWageMap = new Dictionary<string, CompanyEmpWage>();
+    }
+    public void addCompanyEmpWage(string company, int Emp_Rate_Per_Hr, int Num_Of_Working_Days, int Max_Hrs_Per_Month)
+    {
+        CompanyEmpWage companyEmpWage = new CompanyEmpWage(company, Emp_Rate_Per_Hr, Num_Of_Working_Days, Max_Hrs_Per_Month);
+        this.companyEmpWageList.AddLast(companyEmpWage);
+        this.companyToEmpWageMap.Add(company, companyEmpWage);
+    }
+    public void computeEmpWage()
+    {
+        foreach (CompanyEmpWage companyEmpWage in this.companyEmpWageList)
+        {
+            companyEmpWage.setTotalEmpWage(this.computeEmpWage(companyEmpWage));
+            Console.WriteLine(companyEmpWage.toString());
         }
     }
-
-    public class Empcheack
+    private int computeEmpWage(CompanyEmpWage companyEmpWage)
     {
-        public const int isPartTime = 1;
-        public const int isFullTime = 2;
-        public const int empRatePerHr = 20;
+        //variables
+        int empHrs = 0;
+        int totalEmpHrs = 0;
+        int totalWorkingDays = 0;
 
-        public void EmpWage()
+        //Computation
+        while (totalEmpHrs <= companyEmpWage.Max_Hrs_Per_Month && totalWorkingDays < companyEmpWage.Num_Of_Working_Days)
         {
-            int empHr = 0;
-            int empWage = 0;
+            totalWorkingDays++;
             Random random = new Random();
             int empCheck = random.Next(0, 3);
             switch (empCheck)
             {
-                case isPartTime:
-                    empHr = 4;
+                case IS_PART_TIME:
+                    empHrs = 4;
                     break;
-                case isFullTime:
-                    empHr = 8;
+                case IS_FULL_TIME:
+                    empHrs = 8;
                     break;
                 default:
-                    empHr = 0;
+                    empHrs = 0;
                     break;
             }
-            empWage = empHr * empRatePerHr;
-
-            Console.WriteLine("EmpWage=" + empWage);
+            totalEmpHrs = totalEmpHrs + empHrs;
+            Console.WriteLine("Days:" + totalWorkingDays + " Emp Hrs : " + empHrs);
         }
+        return totalEmpHrs * companyEmpWage.Emp_Rate_Per_Hr;
+    }
+    public int getTotalWage(string company)
+    {
+        return this.companyToEmpWageMap[company].totalEmpWage;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+        empWageBuilder.addCompanyEmpWage("TCS", 22, 3, 15);
+        empWageBuilder.addCompanyEmpWage("Jio", 15, 3, 20);
+        empWageBuilder.computeEmpWage();
+        Console.WriteLine("Total wage for TCS company : " + empWageBuilder.getTotalWage("TCS"));
+        Console.WriteLine("Total wage for Jio company : " + empWageBuilder.getTotalWage("Jio"));
     }
 }
