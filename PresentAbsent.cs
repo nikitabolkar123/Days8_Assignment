678465ce1383bbccf747ca9b21e7740ab8fe5475
nikitabolkar123 <bolkarnikita2001@gmail.com>
2022-11-01 06:08:13 -0700

    added uc3

--- PresentAbsent.cs
+++ PresentAbsent.cs
@@ -1,24 +1,33 @@
 ﻿using System;
 
-namespace DailyWage
+namespace PartTimeWage
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Emp e = new Emp();
-            e.Atttendece();
+            EmpCheack emp = new EmpCheack();
+            emp.EmpWage();
         }
     }
-    class Emp
+
+    public class EmpCheack
     {
-        int wagePerHr = 20;
-        int wage = 0, empHr = 0;
-        public void Atttendece()
+        int isPartTime = 1;
+        int isFullTime = 2;
+        int empRatePerHr = 20;
+        int empHr = 0;
+        int empWage = 0;
+
+        public void EmpWage()
         {
             Random random = new Random();
-            int check = random.Next(0, 2);
-            if (check == 0)
+            int empCheck = random.Next(0, 3);
+            if (empCheck == isPartTime)
+            {
+                empHr = 4;
+            }
+            else if (empCheck == isFullTime)
             {
                 empHr = 8;
             }
@@ -26,10 +35,9 @@ namespace DailyWage
             {
                 empHr = 0;
             }
-
-            wage = wagePerHr * empHr;
-            Console.WriteLine("Total Wage=" + wage);
-
+            empWage = empHr * empRatePerHr;
+            Console.WriteLine("EmpWage=" + empWage);
         }
     }
+
 }
