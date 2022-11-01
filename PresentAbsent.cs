4d8271047e4a67f479f26a270f93f2f81e5b06c4
nikitabolkar123 <bolkarnikita2001@gmail.com>
2022-11-01 05:43:52 -0700

    added empwage

---
+++ PresentAbsent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmpWagesPblm
+{
+    internal class PresentAbsent
+    {
+        static void Main(string[] args)
+        {
+            Emp e = new Emp();
+            e.Atttendece();
+        }
+    }
+    class Emp
+    {
+        public void Atttendece()
+        {
+            Random r = new Random();
+            int value = r.Next(0, 2);
+            // 0 is for present;
+            // 1 is for absent;
+            if (value == 0)
+            {
+                Console.WriteLine("Present");
+            }
+            else
+            {
+                Console.WriteLine("Absent");
+            }
+        }
+    }
+}
+   
